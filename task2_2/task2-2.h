4cdbd5c13e652f8deea5dcd60f5d62329625b1b5
xxxFilosoFxxx <48560075+xxxFilosoFxxx@users.noreply.github.com>
2019-03-28 23:46:47 +0300

    'version3.1'

--- task2_2/task2-2.h
+++ task2_2/task2-2.h
@@ -1,4 +1,4 @@
-// Куча на основе указателей с операциями:
+// 9. Куча на основе указателей с операциями:
 // Вставки элемента, извлечение элемента и изменение приоритета элемента.
 
 #ifndef TASK2_2_TASK2_2_H
@@ -12,54 +12,70 @@
 
 using namespace std;
 
+template <typename T>
 struct Node
 {
     Node* parent;
     Node* right;
     Node* left;
+    T value;
     int key;
 
     Node():left(nullptr), right(nullptr), parent(nullptr) {}
-    Node(int _key):key(_key), left(nullptr), right(nullptr), parent(nullptr) {}
-    Node(Node* par, int _key):key(_key), left(nullptr), right(nullptr), parent(par) {}
+    Node(int _key, T _value):key(_key), value(_value), left(nullptr), right(nullptr), parent(nullptr) {}
+    Node(Node* par, int _key, T _value):key(_key), value(_value), left(nullptr), right(nullptr), parent(par) {}
 };
 
+template <typename T>
 class Heap
 {
 public:
     int top = 0;
-    Node* root;
+    Node<T>* root;
 
     Heap():top(0) {}
-    Heap(int _key):top(1) { root = new Node(_key); }
+    Heap(int _key, T _value):top(1) { root = new Node<T>(_key, _value); }
     ~Heap();
     int heap_size();
     int max_key();
+    T max_value();
 
-    void Max_Heapify(Node* A);
-    void Heap_Increase_Key(Node* A, int i);
-    void Max_Heap_Insert(int i);
+    void Max_Heapify(Node<T>* A);
+    void Heap_Increase_Key(Node<T>* A, int i);
+    void Max_Heap_Insert(int i, T _value);
     int Heap_Extract_Max();
-    void Heap_Change_key(Node* A, int i);
+    void Heap_Change_key(Node<T>* A, int i);
     vector<bool> to_bin (int n);
 };
 
-int Heap::heap_size()
+//Реализация методов класса Кучи
+
+template <typename T>
+int Heap<T>::heap_size()
 {
     return top;
 }
 
+template<typename T>
+T Heap<T>::max_value() {
+    if (top == 0)
+        throw "Nedopustimo!";
+    return root->value;
 
-int Heap::max_key() {
-    return root->key;
 }
 
 
-void Heap::Max_Heapify(Node* A)
+template <typename T>
+int Heap<T>::max_key() {
+    return root->key;
+}
+
+template <typename T>
+void Heap<T>::Max_Heapify(Node<T>* A)
 {
-    Node* l = A->left;
-    Node* r = A->right;
-    Node* largest = new Node();
+    Node<T>* l = A->left;
+    Node<T>* r = A->right;
+    Node<T>* largest = new Node<T>();
     if (l != nullptr || r != nullptr)
     {
         if(l != nullptr) {
@@ -80,12 +96,18 @@ void Heap::Max_Heapify(Node* A)
             tmp = A->key;
             A->key = largest->key;
             largest->key = tmp;
+
+            T _tmp;
+            _tmp = A->value;
+            A->value = largest->value;
+            largest->value = _tmp;
             Max_Heapify(largest);
         }
     }
 }
 
-void Heap::Heap_Increase_Key(Node* A, int i) {
+template <typename T>
+void Heap<T>::Heap_Increase_Key(Node<T>* A, int i) {
     while (A->parent != nullptr && A->key < i )
     {
         int tmp;
@@ -95,11 +117,21 @@ void Heap::Heap_Increase_Key(Node* A, int i) {
             tmp = A->left->key;
             A->left->key = A->key;
             A->key = tmp;
+
+            T _tmp = A->left->value;
+            A->left->value = A->value;
+            A->value = _tmp;
+
+
             if(A->left->key > A->parent->right->key)
             {
                 int tmp1 = A->left->key;
                 A->left->key = A->parent->right->key;
                 A->parent->right->key = tmp1;
+
+                T tmp2 = A->left->value;
+                A->left->value = A->parent->right->value;
+                A->parent->right->value = tmp2;
             }
         }
         if (A->right != nullptr && A->right->key == i)
@@ -107,14 +139,22 @@ void Heap::Heap_Increase_Key(Node* A, int i) {
             tmp = A->right->key;
             A->right->key = A->key;
             A->key = tmp;
+
+            T _tmp = A->right->value;
+            A->right->value = A->value;
+            A->value = _tmp;
             if(A->right->key > A->parent->right->key)
             {
                 int tmp1 = A->right->key;
                 A->right->key = A->parent->right->key;
                 A->parent->right->key = tmp1;
+
+                T tmp2 = A->right->value;
+                A->right->value = A->parent->right->value;
+                A->parent->right->value = tmp2;
             }
         }
-        Max_Heapify(root);
+        //  Max_Heapify(root);
         A = A->parent;
     }
     if (A->parent == nullptr && A->right != nullptr )
@@ -125,6 +165,11 @@ void Heap::Heap_Increase_Key(Node* A, int i) {
             tmp = A->right->key;
             A->right->key = root->key;
             root->key = tmp;
+
+            T _tmp;
+            _tmp = A->right->value;
+            A->right->value = root->value;
+            root->value = _tmp;
         }
     }
     if (A->parent == nullptr && A->left != nullptr )
@@ -135,11 +180,17 @@ void Heap::Heap_Increase_Key(Node* A, int i) {
             tmp = A->left->key;
             A->left->key = root->key;
             root->key = tmp;
+
+            T _tmp;
+            _tmp = A->left->value;
+            A->left->value = root->value;
+            root->value = _tmp;
         }
     }
 }
 
-vector<bool> Heap::to_bin(int n)
+template <typename T>
+vector<bool> Heap<T>::to_bin(int n)
 {
     vector<bool> vec;
     while (n > 0)
@@ -150,17 +201,18 @@ vector<bool> Heap::to_bin(int n)
     return vec;
 }
 
-void Heap::Max_Heap_Insert(int i) {
+template <typename T>
+void Heap<T>::Max_Heap_Insert(int i, T _value) {
     if (top == 0)
     {
-        Node* node = new Node(i);
+        Node<T>* node = new Node<T>(i, _value);
         root = node;
     }
     top++;
 
     if (top > 1)
     {
-        Node* node = root;
+        Node<T>* node = root;
         vector<bool> v = to_bin(top);
         for (int i = v.size() - 2; i > 0; --i) {
             if (v[i] == 1)
@@ -169,7 +221,7 @@ void Heap::Max_Heap_Insert(int i) {
                 node = node->left;
         }
 
-        Node* new_node = new Node(node, i);
+        Node<T>* new_node = new Node<T>(node, i, _value);
         if (v[0] == 1)
             node->right = new_node;
         else
@@ -179,7 +231,8 @@ void Heap::Max_Heap_Insert(int i) {
     }
 }
 
-int Heap::Heap_Extract_Max() {
+template <typename T>
+int Heap<T>::Heap_Extract_Max() {
     if (!top) throw "Куча пуста!";
     int max = root->key;
 
@@ -189,7 +242,7 @@ int Heap::Heap_Extract_Max() {
         top--;
         return max;
     }
-    Node* pop = root;
+    Node<T>* pop = root;
     vector<bool> v = to_bin(top);
 
     if (root->left != nullptr && root->right == nullptr)
@@ -219,7 +272,11 @@ int Heap::Heap_Extract_Max() {
         pop->key = root->key;
         root->key = tmp;
 
-        Node* parent = pop->parent;
+        T _tmp = pop->value;
+        pop->value = root->value;
+        root->value = _tmp;
+
+        Node<T>* parent = pop->parent;
         if(flag)
             parent->right = nullptr;
         else
@@ -231,13 +288,15 @@ int Heap::Heap_Extract_Max() {
     return max;
 }
 
-Heap::~Heap()
+template <typename T>
+Heap<T>::~Heap()
 {
     while (top != 0)
         Heap_Extract_Max();
 }
 
-void Heap::Heap_Change_key(Node *A, int i)
+template <typename T>
+void Heap<T>::Heap_Change_key(Node<T>* A, int i)
 {
     if (!top) throw "Куча пуста!";
 
@@ -248,9 +307,4 @@ void Heap::Heap_Change_key(Node *A, int i)
         Heap_Increase_Key(A, i);
 
 }
-
-
-
-
-
 
--- task2_2/task2-2_test.cpp
+++ task2_2/task2-2_test.cpp
@@ -1,4 +1,4 @@
-//Тесты для задания ПЗ-2, Для файла task2-2.h (в котором содержится реализация Кучи).
+//Тесты для задания ПЗ-2, для файла task2-2.h и task2-2.cpp (в которых содержится реализация Кучи).
 
 #include <iostream>
 #include "cstdio"
@@ -11,46 +11,80 @@
 #include "../catch.hpp"
 
 TEST_CASE("Test1", "task2-2_Tests") {
-    Heap* heap = new Heap(10);
-    heap->Max_Heap_Insert(5);
-    heap->Max_Heap_Insert(12);
-    heap->Max_Heap_Insert(7);
-    heap->Max_Heap_Insert(15);
+    Heap<int>* heap = new Heap<int>(10, 0);
+    heap->Max_Heap_Insert(5, 0);
+    heap->Max_Heap_Insert(12, 0);
+    heap->Max_Heap_Insert(7, 0);
+    heap->Max_Heap_Insert(15, 0);
 
-    heap->Max_Heap_Insert(16);
-    heap->Max_Heap_Insert(17);
-    heap->Max_Heap_Insert(18);
-    heap->Max_Heap_Insert(19);
-    heap->Max_Heap_Insert(8);
-    heap->Max_Heap_Insert(2);
+    heap->Max_Heap_Insert(16, 0);
+    heap->Max_Heap_Insert(17, 5);
+    heap->Max_Heap_Insert(18, 0);
+    heap->Max_Heap_Insert(19, 1);
+    heap->Max_Heap_Insert(8, 0);
+    heap->Max_Heap_Insert(2, 0);
     REQUIRE(heap->max_key() == 19);
+    REQUIRE(heap->max_value() == 1);
 
     heap->Heap_Extract_Max();
     heap->Heap_Extract_Max();
     REQUIRE(heap->max_key() == 17);
+    REQUIRE(heap->max_value() == 5);
 }
 
 TEST_CASE("Test2", "task2-2_Tests") {
-    Heap* heap = new Heap(10);
-    heap->Max_Heap_Insert(8);
+    Heap<int>* heap = new Heap<int>(10, 1);
+    heap->Max_Heap_Insert(8, 2);
+    heap->Max_Heap_Insert(7, 3);
+    heap->Max_Heap_Insert(6, 4);
+    REQUIRE(heap->max_value() == 1);
     REQUIRE(heap->Heap_Extract_Max() == 10);
+    REQUIRE(heap->max_value() == 2);
     REQUIRE(heap->Heap_Extract_Max() == 8);
+    REQUIRE(heap->max_value() == 3);
+    REQUIRE(heap->Heap_Extract_Max() == 7);
+    REQUIRE(heap->max_value() == 4);
+    REQUIRE(heap->Heap_Extract_Max() == 6);
+    REQUIRE_THROWS(heap->max_value());
     REQUIRE_THROWS(heap->Heap_Extract_Max());
     REQUIRE_THROWS(heap->Heap_Change_key(heap->root, 0));
 }
 
 TEST_CASE("Test3", "task2-2_Tests")
 {
-    Heap* heap = new Heap(10);
-    heap->Max_Heap_Insert(5);
-    heap->Max_Heap_Insert(12);
-    heap->Max_Heap_Insert(2);
+    Heap<int>* heap = new Heap<int>(10, 0);
+    heap->Max_Heap_Insert(5, 0);
+    heap->Max_Heap_Insert(12, 0);
+    heap->Max_Heap_Insert(2, 0);
     heap->Heap_Change_key(heap->root->left->left, 20);
     REQUIRE(heap->Heap_Extract_Max() == 20);
     for (int i = 0; i < 3; i++)
         heap->Heap_Extract_Max();
-    heap->Max_Heap_Insert(12);
+    heap->Max_Heap_Insert(12, 0);
     heap->Heap_Extract_Max();
     REQUIRE_THROWS( heap->Heap_Extract_Max());
-
 }
+
+TEST_CASE("Test4", "task2-2_Tests") {
+    Heap<char>* heap = new Heap<char>(10, '0');
+    heap->Max_Heap_Insert(5, '0');
+    heap->Max_Heap_Insert(12, '0');
+    heap->Max_Heap_Insert(7, '0');
+    heap->Max_Heap_Insert(15, '0');
+
+    heap->Max_Heap_Insert(16, '0');
+    heap->Max_Heap_Insert(17, '5');
+    heap->Max_Heap_Insert(18, '0');
+    heap->Max_Heap_Insert(19, '1');
+    heap->Max_Heap_Insert(8, '0');
+    heap->Max_Heap_Insert(2, '0');
+    REQUIRE(heap->max_key() == 19);
+    REQUIRE(heap->max_value() == '1');
+
+    heap->Heap_Extract_Max();
+    heap->Heap_Extract_Max();
+    REQUIRE(heap->max_key() == 17);
+    REQUIRE(heap->max_value() == '5');
+}
+
+
